830a617d02edda12e24029f6504d4b12090dffe7
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-12-15 14:38:29 +0300

    add Router

--- src/services/instaService.js
+++ src/services/instaService.js
@@ -18,5 +18,17 @@ export default class instaService{
         const res = await this.getResource('/posts/');
         return res
     }
+
+    getAllPhotos = async () =>{
+        const res = await this.getResource('/posts/');
+        return res.map(this._transformPosts)
+    }
+
+    _transformPosts = (post) =>{
+        return{
+            src: post.src,
+            alt: post.alt
+        }
+    }
 }
 
