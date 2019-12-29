2f84bdc7b3623d5e266ebe9907bf5b4713d083af
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-12-29 21:12:08 +0300

    Updates

---
+++ precache-manifest.2ebb16b76d9a638d83d3742c3fd7e1e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1144bbe38a9d9671472dca310ace5c",
+    "url": "/react_inst/index.html"
+  },
+  {
+    "revision": "e83a4c605694578c087e",
+    "url": "/react_inst/static/css/main.87d432a9.chunk.css"
+  },
+  {
+    "revision": "c1ca682fe3fbe44dda16",
+    "url": "/react_inst/static/js/2.0362b095.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react_inst/static/js/2.0362b095.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e83a4c605694578c087e",
+    "url": "/react_inst/static/js/main.5d0127f3.chunk.js"
+  },
+  {
+    "revision": "77c30926f9b08585a966",
+    "url": "/react_inst/static/js/runtime-main.95143c84.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/react_inst/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/react_inst/static/media/logo.9e8a2b39.svg"
+  }
+]);
